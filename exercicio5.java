0f1d4d903229ed91a82848c47422a2fa746f5a44
LuanAngeloRocha <Luanrochaangelo@gmail.com>
2021-08-26 16:59:34 +0000

    atualizando exercicio 5

---
+++ exercicio5.java
@@ -0,0 +1,16 @@
+public class exercicio5 {
+    public static void main (String args[]){
+        int x  = 58 ;
+        if (x%2 == 0){
+            x = x/2;
+            System.out.println("O valor de x é :"+x);
+
+        }
+        else {
+            x = 3* x + 1;
+            System.out.println("O valor de x é :"+x);
+            
+        }
+
+    }
+}
